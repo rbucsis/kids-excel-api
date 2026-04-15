94f8f6db0bd95c15a7bcc89dad2c595541003de8
Robert Bucsis <rbucsis@gmail.com>
2026-04-15 08:51:49 -0600

    Handling student get endpoints

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from dotenv import load_dotenv
 import pg
 
@@ -6,10 +7,24 @@ load_dotenv()
 
 app = FastAPI()
 
+origins = [
+    "http://localhost:3000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 @app.get("/students")
 def get_students():
+    print("Fetching Students...")
     return pg.read('students')
 
 @app.get("/students/{id}")
 def get_student(id: str):
+    print("Fetching Student "+id+"...")
     return pg.read('students', id)
